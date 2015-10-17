3b5359822e1b35ecc1411e299aa66f5ccb15b801
momomoxiaoxi <2759696814@qq.com>
2015-10-17 13:52:39 +0800

    Update config.py

--- www/config.py
+++ www/config.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
+"""
 Configuration
-'''
+"""
 
-__author__ = 'Michael Liao'
+from transwarp.db import Dict
 
 import config_default
 
-class Dict(dict):
-    '''
-    Simple dict but support access as x.y style.
-    '''
-    def __init__(self, names=(), values=(), **kw):
-        super(Dict, self).__init__(**kw)
-        for k, v in zip(names, values):
-            self[k] = v
-
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
-
-    def __setattr__(self, key, value):
-        self[key] = value
 
 def merge(defaults, override):
+    """
+    合并override 和 default 配置文档，返回字典
+    """
     r = {}
     for k, v in defaults.iteritems():
         if k in override:
@@ -39,13 +25,19 @@ def merge(defaults, override):
             r[k] = v
     return r
 
+
 def toDict(d):
+    """
+    将一个字典对象转换成 一个Dict对象
+    """
     D = Dict()
     for k, v in d.iteritems():
         D[k] = toDict(v) if isinstance(v, dict) else v
     return D
 
 configs = config_default.configs
+
+
 
 try:
     import config_override
