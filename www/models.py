eb5c1e33817889d4de43dbadf07109bb44e47448
momomoxiaoxi <2759696814@qq.com>
2015-10-19 19:30:12 +0800

    修改里models里面的输入错误和重复

--- www/models.py
+++ www/models.py
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-'''
-Models for user, blog, comment.
-'''
-
-import time, uuid
-
-from transwarp.db import next_id
-from transwarp.orm import Model, StringField, BooleanField, FloatField, TextField
-
-class User(Model):
-    __table__ = 'users'
-
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    email = StringField(updatable=False, ddl='varchar(50)')
-    password = StringField(ddl='varchar(50)')
-    admin = BooleanField()
-    name = StringField(ddl='varchar(50)')
-    image = StringField(ddl='varchar(500)')
-    created_at = FloatField(updatable=False, default=time.time)
-
-class Blog(Model):
-    __table__ = 'blogs'
-
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    user_id = StringField(updatable=False, ddl='varchar(50)')
-    user_name = StringField(ddl='varchar(50)')
-    user_image = StringField(ddl='varchar(500)')
-    name = StringField(ddl='varchar(50)')
-    summary = StringField(ddl='varchar(200)')
-    content = TextField()
-    created_at = FloatField(updatable=False, default=time.time)
-
-class Comment(Model):
-    __table__ = 'comments'
-
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    blog_id = StringField(updatable=False, ddl='varchar(50)')
-    user_id = StringField(updatable=False, ddl='varchar(50)')
-    user_name = StringField(ddl='varchar(50)')
-    user_image = StringField(ddl='varchar(500)')
-    content = TextField()
-    created_at = FloatField(updatable=False, default=time.time)
-=======
 #!usr/bin/env python
 # -*- coding:utf-8 -*-
 
@@ -60,7 +13,7 @@ mysql -u root -p < schema.sql
 import time,uuid
 
 from transwarp.db import next_id
-from transwarp.orm import Model,StringFielf,BooleanField,TextField
+from transwarp.orm import Model,StringField,BooleanField,FloatField,TextField
 
 class User(Model):
 	__table__ = 'users'
@@ -70,29 +23,28 @@ class User(Model):
 	password =StringField(ddl='varchar(50)')
 	admin = BooleanField()
 	name = StringField(ddl='varchar(50)')
-	image = StringFiel(ddl='varchar(500)')
+	image = StringField(ddl='varchar(500)')
 	created_at = FloatField(updatable=False, default=time.time)
 
 class Blog(Model):
 	__table__ = 'blogs'
 
 	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    user_id = StringField(updatable=False, ddl='varchar(50)')
-    user_name = StringField(ddl='varchar(50)')
-    user_image = StringField(ddl='varchar(500)')
-    name = StringField(ddl='varchar(50)')
-    summary = StringField(ddl='varchar(200)')
-    content = TextField()
-    created_at = FloatField(updatable=False, default=time.time)
+        user_id = StringField(updatable=False, ddl='varchar(50)')
+        user_name = StringField(ddl='varchar(50)')
+        user_image = StringField(ddl='varchar(500)')
+        name = StringField(ddl='varchar(50)')
+        summary = StringField(ddl='varchar(200)')
+        content = TextField()
+        created_at = FloatField(updatable=False, default=time.time)
 
 class Comment(Model):
-    __table__ = 'comments'
+        __table__ = 'comments'
 
-    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    blog_id = StringField(updatable=False, ddl='varchar(50)')
-    user_id = StringField(updatable=False, ddl='varchar(50)')
-    user_name = StringField(ddl='varchar(50)')
-    user_image = StringField(ddl='varchar(500)')
-    content = TextField()
-    created_at = FloatField(updatable=False, default=time.time)
->>>>>>> day_5
+        id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+        blog_id = StringField(updatable=False, ddl='varchar(50)')
+        user_id = StringField(updatable=False, ddl='varchar(50)')
+        user_name = StringField(ddl='varchar(50)')
+        user_image = StringField(ddl='varchar(500)')
+        content = TextField()
+        created_at = FloatField(updatable=False, default=time.time)
